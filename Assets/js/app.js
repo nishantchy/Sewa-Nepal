74a20a232df8cb9f67df094d28e45e383eda9e17
nishantchy <nishantchy1234@gmail.com>
2024-02-18 13:21:48 +0545

    folder structure changes

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll" , () => {
     const header = document.getElementById("header");
     
-    if (window.scrollY > 600) {
+    if (window.scrollY > 400) {
         header.classList.add("header-bg")
     } else {
         header.classList.remove("header-bg")
