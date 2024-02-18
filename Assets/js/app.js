ceb3d7dfbee752723f6f4a50e00f660fac5f7212
nishantchy <nishantchy1234@gmail.com>
2024-02-18 18:05:36 +0545

    responsive footer

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -8,3 +8,17 @@ window.addEventListener("scroll" , () => {
     }
 })
 
+const boxes = document.querySelectorAll(".content");
+const checkBoxes = () =>{
+    const triggerBottom = (window.innerHeight/5)*4;
+    boxes.forEach((box)=> {
+        const boxTop = box.getBoundingClientRect().top;
+        if(boxTop < triggerBottom)box.classList.add("show");
+        else box.classList.remove("show");
+    })
+}
+window.addEventListener("scroll",checkBoxes);
+checkBoxes();
+
+
+
